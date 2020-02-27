77175b72639ad9793587602a7cf012b834646386
birdsoftheworld <exoticbirdsoftheworld@gmail.com>
2020-02-27 07:49:47 -0800

    Make ListSpecialties return camelcase names

--- src/main/java/com/github/birdsoftheworld/specialloot/commands/ListSpecialties.java
+++ src/main/java/com/github/birdsoftheworld/specialloot/commands/ListSpecialties.java
@@ -11,7 +11,7 @@ public class ListSpecialties implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         for(Specialty specialty : Specialties.values()) {
-            sender.sendMessage(ChatColor.BOLD.toString() + ChatColor.GREEN.toString() + specialty.getName().toLowerCase());
+            sender.sendMessage(ChatColor.BOLD.toString() + ChatColor.GREEN.toString() + specialty.getName());
             sender.sendMessage(specialty.getProperties().getLoreRaw());
         }
         return true;
